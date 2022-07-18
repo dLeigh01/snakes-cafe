964db7cec942c5b2bc68ca7e27b01a265e4e14bb
Dana Huffman <dana.huffman@yahoo.com>
2022-07-18 14:26:58 -0700

    Added order summary and menu list

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -37,24 +37,30 @@ Unicorn Tears
 """)
 # Initialize empty dictionary
 meal = {}
+menu = ["Wings", "Cookies", "Spring Rolls", "Salmon", "Steak", "Meat Tornado", "A Literal Garden", "Ice Cream", "Cake", "Pie", "Coffee", "Tea", "Unicorn Tears"] # all possible orders
 
 # Loop through ordering until user enters 'quit'
 # Create a variable to store user's order
 order = input("> ")
+
 while order != "quit":
+    while order not in menu:
+        print("""
+        ** I'm sorry, I didn't recognize that item... **
+        ** please check that it was spelled correctly **
+        """)
+        order = input("> ")
     if order in meal:
         meal[order] += 1
     else:
         meal[order] = 1
 
     # Create order report
+    for item in meal:
+        if meal[item] == 1:
+            print(f"** {meal[item]} order of {item} has been added to your meal **")
+        else:
+            print(f"** {meal[item]} orders of {item} have been added to your meal **")
 
-    if meal[order] == 1:
-        report = f"** {meal[order]} order of {order} has been added to your meal **"
-    else:
-        report = f"** {meal[order]} orders of {order} have been added to your meal **"
-
-    # print user's current order
-    print(report)
     order = input("> ")
 # end loop
