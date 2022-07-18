f4761fa40e625afa973eb0caa9372f63a8183b6a
Dana Huffman <dana.huffman@yahoo.com>
2022-07-18 14:00:01 -0700

    Minimum functionality completed

---
+++ README.md
@@ -0,0 +1,15 @@
+# Lab - Class 01
+
+## Project: Snakes Cafe
+
+### Author: Dana Huffman
+
+### Links and Resources
+
+https://www.w3schools.com/python/python_dictionaries.asp
+
+https://appdividend.com/2021/03/31/how-to-add-values-to-dictionary-in-python/
+
+https://stackoverflow.com/questions/1602934/check-if-a-given-key-already-exists-in-a-dictionary
+
+### Setup
--- snakes_cafe.py
+++ snakes_cafe.py
@@ -36,17 +36,25 @@ Unicorn Tears
 ***********************************
 """)
 # Initialize empty dictionary
-Meal = {}
+meal = {}
 
 # Loop through ordering until user enters 'quit'
-
 # Create a variable to store user's order
 order = input("> ")
 while order != "quit":
-    # print user's current order
-    num_items = 1 # TODO: tally items
-    report = f"** {num_items} order of {order} have been added to your meal **"
+    if order in meal:
+        meal[order] += 1
+    else:
+        meal[order] = 1
 
+    # Create order report
+
+    if meal[order] == 1:
+        report = f"** {meal[order]} order of {order} has been added to your meal **"
+    else:
+        report = f"** {meal[order]} orders of {order} have been added to your meal **"
+
+    # print user's current order
     print(report)
     order = input("> ")
 # end loop
